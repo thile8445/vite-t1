4245ec686927774195c5145d28023773dbfbfe07
thilnbap <thiln@bap.jp>
2024-07-19 16:41:25 +0700

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import Search from "./pages/search";
+import Layout from "./layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Route>{" "}
       </Routes>
     </BrowserRouter>
   );
